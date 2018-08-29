64d23878acae0f5df0b1728b30cf0860511b93a3
AFRAIN CALIXTO <calixtoafrain@gmail.com>
2018-08-29 13:21:41 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/udemy/curso/services/CategoriaService.java
+++ src/main/java/com/udemy/curso/services/CategoriaService.java
@@ -1,36 +1,48 @@
- package com.udemy.curso.services;
+package com.udemy.curso.services;
 
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.udemy.curso.domain.Categoria;
 import com.udemy.curso.repositories.CategoriaRepository;
+import com.udemy.curso.services.exceptions.DataIntegrityException;
 import com.udemy.curso.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
 
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		
-		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: "+ id 
-				+ ", Tipo: " + Categoria.class.getName()));
-		
+
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
-	
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma Categória "
+					+ "que possui produtos vinculados a ela!");
+		}
+	}
+
 }
