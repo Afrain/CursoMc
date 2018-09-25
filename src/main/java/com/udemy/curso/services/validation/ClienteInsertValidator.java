0a6f1610ac4b4a6d2055c3e63c44d37205c11f66
AFRAIN CALIXTO <calixtoafrain@gmail.com>
2018-09-25 06:55:45 -0300

    email não repetido na inserção do cliente

--- src/main/java/com/udemy/curso/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/udemy/curso/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.udemy.curso.domain.Cliente;
 import com.udemy.curso.domain.enums.TipoCliente;
 import com.udemy.curso.dto.ClienteNewDTO;
+import com.udemy.curso.repositories.ClienteRepository;
 import com.udemy.curso.resources.exception.FieldMessage;
 import com.udemy.curso.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -28,6 +35,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("CpfOuCnpj", "CNPJ inválido!"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		if(aux != null) {
+			list.add(new FieldMessage("Email","Email já cadastrado!"));
 		}
 		
 		for (FieldMessage e : list) {
